9c518ba3dae4a956e77ed1f95fa49c9d1a73d07c
Michael Chi <michael.chi1997@gmail.com>
2021-01-14 10:17:28 -0500

    Retrieve all portfolio value

--- simulatorTradingApi/Account.py
+++ simulatorTradingApi/Account.py
@@ -3,15 +3,63 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
 class UserPortfolio:
     def __init__(self):
+        self.retrievePortfolio()
+
+    def retrievePortfolio(self):
         client = HeadlessClient.getInstance()
-        info = client.find_elements_by_class_name('infobar-title')
-        # for info_ele in info:
-        #     accountValueUSD = info_ele.find_element_by_tag_name('p').find_element_by_css_selector(':nth-child(2)')
-        #     self.accountValueUSD = accountValueUSD
-        accountValueUSD = info[0].find_element_by_tag_name('p').find_element_by_css_selector(':nth-child(2)').get_attribute('innerText')
-        self.accountValueUSD = accountValueUSD
+        info = client.find_elements_by_class_name("infobar-title")
+
+        if len(info) < 4:
+            print("Failed to retrieve user portfolio")
+            return
+
+        accountValueUSD = (
+            info[0]
+            .find_element_by_tag_name("p")
+            .find_element_by_css_selector(":nth-child(2)")
+            .get_attribute("innerText")
+        )
+        buyingPower = (
+            info[1]
+            .find_element_by_tag_name("p")
+            .find_element_by_css_selector(":nth-child(2)")
+            .get_attribute("innerText")
+        )
+
+        cash = (
+            info[2]
+            .find_element_by_tag_name("p")
+            .find_element_by_css_selector(":nth-child(2)")
+            .get_attribute("innerText")
+        )
+
+        annualReturn = (
+            info[3]
+            .find_element_by_tag_name("p")
+            .find_element_by_css_selector(":nth-child(2)")
+            .get_attribute("innerText")
+        )
+
+        self.accountValueUSD = self.__extractNumber(accountValueUSD)
+        self.buyingPower = self.__extractNumber(buyingPower)
+        self.cash = self.__extractNumber(cash)
+        self.annualReturn = self.__extractNumber(annualReturn)
+
+    def __extractNumber(self, string):
+        trimmed = string.strip()
+        if "$" in trimmed:
+            return float(trimmed[1:].replace(",", ""))
+        else:
+            return float(trimmed[:-1].replace(",", ""))
+
 
 class Account:
     isLoggedIn = False
@@ -23,19 +71,19 @@ class Account:
 
     def authenticate(self):
         if not self.isLoggedIn:
-                client = HeadlessClient.getInstance()
-                client.get('https://www.investopedia.com/simulator/home.aspx')
-                client.save_screenshot("screenshot.png")
-                loginButton = WebDriverWait(client, 10).until(
-                    EC.presence_of_element_located((By.ID, "login"))
-                )
-                username = client.find_element_by_id('username')
-                password = client.find_element_by_id('password')
-                username.send_keys(self.email)
-                password.send_keys(self.password)
-                loginButton.click()
-                self.isLoggedIn = True
-                self.retrieveUserInfo()
+            client = HeadlessClient.getInstance()
+            client.get("https://www.investopedia.com/simulator/home.aspx")
+            client.save_screenshot("screenshot.png")
+            loginButton = WebDriverWait(client, 10).until(
+                EC.presence_of_element_located((By.ID, "login"))
+            )
+            username = client.find_element_by_id("username")
+            password = client.find_element_by_id("password")
+            username.send_keys(self.email)
+            password.send_keys(self.password)
+            loginButton.click()
+            self.isLoggedIn = True
+            self.retrieveUserInfo()
         else:
             print("You are already authenticated.")
 
@@ -44,10 +92,13 @@ class Account:
             print("You need to login first.")
         else:
             client = HeadlessClient.getInstance()
-            client.get('https://www.investopedia.com/simulator/portfolio/')
-            client.save_screenshot('portfolio.png')
+            client.get("https://www.investopedia.com/simulator/portfolio/")
+            client.save_screenshot("portfolio.png")
             self.user = UserPortfolio()
-            print("account value: ", self.user.accountValueUSD)
+            pp.pprint(self.user.accountValueUSD)
+            pp.pprint(self.user.buyingPower)
+            pp.pprint(self.user.cash)
+            pp.pprint(self.user.annualReturn)
 
     def closeSession(self):
         HeadlessClient.close()
